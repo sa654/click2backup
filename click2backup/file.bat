9fe49ca7ebd08bcf960e4bab3d55698305fb2d36
sa654 <74795457+sa654@users.noreply.github.com>
2021-11-15 21:25:17 +1300

    Add files via upload

---
+++ click2backup/file.bat
@@ -0,0 +1,18 @@
+@echo off
+::variables
+/min
+SET odrive=%drive:~0,2%
+set backupcmd=xcopy /s /c /d /e /h /i /r /y 
+echo off
+%backupcmd%"%USERPROFILE%\Favorites"
+%backupcmd%"%USERPROFILE%\videos"
+%backupcmd%"%USERPROFILE%\Desktop"
+%backupcmd%"%USERPROFILE%\Recycle Bin"
+%backupcmd%"%USERPROFILE%\Downloads"
+%backupcmd% "%USERPROFILE%\Documents"
+%backupcmd% "%USERPROFILE%\Pictures"
+%backupcmd% "%USERPROFILE%\Videos"
+%backupcmd% "%USERPROFILE%\Music"
+
+@echo off
+cls
